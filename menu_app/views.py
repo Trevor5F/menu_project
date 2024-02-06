efafdbeb46a3a46bc39304a2f47047baaedc127f
Admin <Dexter2366@yandex.ru>
2024-02-06 09:54:03 +0300

    init

---
+++ menu_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Menu
+
+
+def index(request):
+    return render(request, 'index.html', {'menus': Menu.objects.all()})
+
+
+def draw_menu(request, path):
+    splitted_path = path.split('/')
+    assert len(splitted_path) > 0, ('= Draw_menu function failed =')
+    return render(request, 'index.html', {'menu_name': splitted_path[0], 'menu_item': splitted_path[-1]})
